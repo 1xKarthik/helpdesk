b657a2aea258494d272b07dbd30c19fefa87ce55
karthikgirraj <karthik.girraj@gmail.com>
2014-11-12 22:46:58 +0530

    agin again

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysql_connect("127.11.109.2","adminiqRrjFd","AwFKEvwcaXFc");
+$con=mysql_connect("127.11.109.2:3306","adminiqRrjFd","AwFKEvwcaXFc");
 mysql_select_db("onlinehelpdesk",$con);
 
 ?>
