005bad5eadef217c4d49dc7210230a9284a4fc3f
karthikgirraj <karthik.girraj@gmail.com>
2014-11-12 22:32:56 +0530

    database

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysql_connect("localhost","root","");
-mysql_select_db("project",$con);
+$con=mysql_connect("https://onlinehelpdesk-vitians.rhcloud.com/phpmyadmin/","adminiqRrjFd","AwFKEvwcaXFc");
+mysql_select_db("onlinehelpdesk",$con);
 
 ?>
