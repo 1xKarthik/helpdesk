80291c1c9fc620a9c36b24f798a42848b4a5d06e
karthikgirraj <karthik.girraj@gmail.com>
2014-11-13 13:28:55 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysql_connect("localhost","adminiqRrjFd","AwFKEvwcaXFc");
-mysql_select_db("project",$con);
+$con=mysql_connect("127.11.55.2:3306","admin842lU4x","UrGCn87kZ2w5");
+mysql_select_db("onlinehelpdk",$con);
 
 ?>
