e25d2acc41fc24781fb48685bad8c3cd263ae7ee
karthikgirraj <karthik.girraj@gmail.com>
2014-11-13 18:34:18 +0530

    oo

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysql_connect("localhost","adminiqRrjFd","AwFKEvwcaXFc");
-mysql_select_db("project",$con);
+$con=mysql_connect("127.11.109.2:3306","adminiqRrjFd","AwFKEvwcaXFc");
+mysql_select_db("onlinehelpdesk",$con);
 
 ?>
